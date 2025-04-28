5997728f60a5cbf1ecffbdff7ddd770319094d84
bigyanjr11 <bigyanbasnet296@gmail.com>
2025-04-28 20:57:13 +0545

    okay

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,34 @@
 #include<stdio.h>
 int main(){
-	printf("Seems Interesting Right?\n");
-	printf("\nFollow For More\n");
+	int num1, num2, result;
+	char op;
+	printf("Enter two numbers: ");
+	scanf("%d %d", &num1, &num2);
+	printf("Enter the Operator: ");
+	scanf(" %c", &op);
+	switch (op)
+	{
+	case '+':
+	result = num1 + num2;
+		break;
+	case '-':
+		result = num1 - num2;
+			break;
+	case '*':
+	result = num1 * num2;
+		break;
+	case '/':
+	if(num2 != 0){
+		result = num1 /num2;
+	} else{
+		printf("Error: Division by Zero");
+	}
+	return 1;
+		break;
+	default:
+	printf("!!--Invalid Operator--!!");
+		break;
+		return 1;
+	}
+return 0;
 }
